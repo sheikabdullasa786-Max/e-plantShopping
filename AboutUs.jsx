9669350b5c459b253ce3e3afb399fa491021739c
sheikabdullasa786-Max <sheikabdullasa786@gmail.com>
2025-12-26 12:58:42 +0530

    Add AboutUs component with nursery description

---
+++ AboutUs.jsx
@@ -0,0 +1,13 @@
+function AboutUs() {
+  return (
+    <div>
+      <h2>About Paradise Nursery</h2>
+      <p>
+        Paradise Nursery is your one-stop shop for healthy and beautiful plants.
+        We offer indoor and outdoor plants to bring nature closer to you.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
